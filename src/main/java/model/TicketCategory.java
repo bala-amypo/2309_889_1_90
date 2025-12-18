2f1280a07f3196692f85bd3d98efe03ebc387ef5
bala-amypo <bala@amypo.in>
2025-12-18 09:18:58 +0000

    User commit

--- src/main/java/model/TicketCategory.java
+++ src/main/java/model/TicketCategory.java
@@ -6,4 +6,38 @@ public class TicketCategory{
     private String categoryName;
     private String description;
     private LocalDateTime createdAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCategoryName() {
+        return categoryName;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public TicketCategory(Long id, String categoryName, String description, LocalDateTime createdAt) {
+        this.id = id;
+        this.categoryName = categoryName;
+        this.description = description;
+        this.createdAt = createdAt;
+    }
+    public TicketCategory() {
+    }
+
+
 }
