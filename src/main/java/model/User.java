d7033fd78cea086cd00330e8b16fa82d14453c41
bala-amypo <bala@amypo.in>
2025-12-18 08:33:22 +0000

    User commit

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,5 +2,10 @@ package com.example.domo.model;
 
 
 public class User{
+    private Long id;
+    private string fullName;
+    @Column(unique=true)
+    private string email;
+    private string password;
     private 
 }
