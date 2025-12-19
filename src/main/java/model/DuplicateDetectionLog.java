9fd155e2d66ddcda6023f9534706881ad02bf8c6
bala-amypo <bala@amypo.in>
2025-12-19 04:25:18 +0000

    User commit

--- src/main/java/model/DuplicateDetectionLog.java
+++ src/main/java/model/DuplicateDetectionLog.java
@@ -5,5 +5,30 @@ public class DuplicateDetectionLog{
     private Long id;
     private Double matchScore;
     private LocalDateTime detectedAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Double getMatchScore() {
+        return matchScore;
+    }
+    public void setMatchScore(Double matchScore) {
+        this.matchScore = matchScore;
+    }
+    public LocalDateTime getDetectedAt() {
+        return detectedAt;
+    }
+    public void setDetectedAt(LocalDateTime detectedAt) {
+        this.detectedAt = detectedAt;
+    }
+    public DuplicateDetectionLog(Long id, Double matchScore, LocalDateTime detectedAt) {
+        this.id = id;
+        this.matchScore = matchScore;
+        this.detectedAt = detectedAt;
+    }
+    public DuplicateDetectionLog() {
+    }
     
 }
