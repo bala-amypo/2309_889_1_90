d74b6c2a74b85fb0ebd7a09eea2c29e0ad969ede
bala-amypo <bala@amypo.in>
2025-12-19 04:34:18 +0000

    User commit

--- src/main/resources/DuplicateDetectionLogRepository.java
+++ src/main/resources/DuplicateDetectionLogRepository.java
@@ -2,6 +2,6 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.DuplicateDetectionLog;
-public interface DeliveryEvaluationRepository extends JpaRepository<DeliveryEvaluation,Long>{
+public interface DuplicateDetectionLogRepository extends JpaRepository<DuplicateDetectionLog,Long>{
     
 }
