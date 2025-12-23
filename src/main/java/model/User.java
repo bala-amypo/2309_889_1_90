6c5c9c058a656e10696a6d5309382adf380b896a
bala-amypo <bala@amypo.in>
2025-12-23 06:26:48 +0000

    User commit

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -6,65 +6,65 @@ import java.util.List;
 @Table(name = "users")
 public class User {
  @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private Long id;
- private String fullName;
- @Column(unique = true)
- private String email;
- private String password;
- private String role;
- private LocalDateTime createdAt;
- @OneToMany(mappedBy = "user")
- private List<Ticket> tickets;
- public User() {
- }
- public User(String fullName, String email, String password, String
-role) {
- this.fullName = fullName;
- this.email = email;
- this.password = password;
- this.role = (role == null) ? "USER" : role;
- }
- public Long getId() {
- return id;
- }
- public void setId(Long id) {
- this.id = id;
- }
- public String getFullName() {
- return fullName;
- }
- public void setFullName(String fullName) {
- this.fullName = fullName;
- }
- public String getEmail() {
- return email;
- }
- public void setEmail(String email) {
- this.email = email;
- }
- public String getPassword() {
- return password;
- }
- public void setPassword(String password) {
- this.password = password;
- }
- public String getRole() {
- return role;
- }
- public void setRole(String role) {
- this.role = role;
- }
- public LocalDateTime getCreatedAt() {
- return createdAt;
- }
- public void setCreatedAt(LocalDateTime createdAt) {
- this.createdAt = createdAt;
- }
- public List<Ticket> getTickets() {
- return tickets;
- }
- public void setTickets(List<Ticket> tickets) {
- this.tickets = tickets;
- }
-}
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+    private String fullName;
+     @Column(unique = true)
+      private String email;
+       private String password;
+        private String role;
+         private LocalDateTime createdAt;
+          @OneToMany(mappedBy = "user")
+           private List<Ticket> tickets;
+            public User() {
+             }
+              public User(String fullName, String email, String password, String
+              role) {
+               this.fullName = fullName;
+                this.email = email;
+                 this.password = password;
+                  this.role = (role == null) ? "USER" : role;
+                   }
+                    public Long getId() {
+                     return id;
+                      }
+                       public void setId(Long id) {
+                        this.id = id;
+                         }
+                          public String getFullName() {
+                           return fullName;
+                            }
+                             public void setFullName(String fullName) {
+                              this.fullName = fullName;
+                               }
+                                public String getEmail() {
+                                 return email;
+                                  }
+                                   public void setEmail(String email) {
+                                    this.email = email;
+                                     }
+                                      public String getPassword() {
+                                       return password;
+                                        }
+                                         public void setPassword(String password) {
+                                          this.password = password;
+                                           }
+                                            public String getRole() {
+                                             return role;
+                                              }
+                                               public void setRole(String role) {
+                                                this.role = role;
+                                                 }
+                                                  public LocalDateTime getCreatedAt() {
+                                                   return createdAt;
+                                                    }
+                                                     public void setCreatedAt(LocalDateTime createdAt) {
+                                                      this.createdAt = createdAt;
+                                                       }
+                                                        public List<Ticket> getTickets() {
+                                                         return tickets;
+                                                          }
+                                                           public void setTickets(List<Ticket> tickets) {
+                                                            this.tickets = tickets;
+                                                             }
+                                                             }
