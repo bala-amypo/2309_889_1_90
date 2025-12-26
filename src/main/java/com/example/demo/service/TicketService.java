26e97342bbb57b8c4b7ac36cf900dae2372678c2
bala-amypo <bala@amypo.in>
2025-12-26 17:34:24 +0000

    User commit

--- src/main/java/com/example/demo/service/TicketService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-import com.example.demo.model.Ticket;
-import java.util.List;
-public interface TicketService {
- Ticket createTicket(Long userId, Long categoryId, Ticket ticket);
- Ticket getTicket(Long ticketId);
- List<Ticket> getTicketsByUser(Long userId);
- List<Ticket> getAllTickets();
-}
